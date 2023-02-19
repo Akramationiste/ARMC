fc9a25b16968f472c571222ca0270f6432b023dc
Akramationiste <akram.chougara@gmail.com>
2023-02-19 15:30:45 +0100

    db connected

--- Index.js
+++ Index.js
@@ -1,14 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose')
 
-const uri = "mongodb+srv://chanelhm50:iPqIxFQtJrkHRqkW@cluster0.q8id5cn.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-
-client.connect(err => {
-  if (err) {
-    console.log('Impossible de se connecter à la base de données', err);
-  } else {
-    console.log('Connecté à la base de données');
-    const collection = client.db("test").collection("devices");
-    // Effectuez des opérations sur la collection ici
-  }
-});
+mongoose.connect("mongodb+srv://chanelhm50:iPqIxFQtJrkHRqkW@cluster0.q8id5cn.mongodb.net/?retryWrites=true&w=majority").then(res => {
+    console.log('connected')
+}).catch(err => console.log(err))
